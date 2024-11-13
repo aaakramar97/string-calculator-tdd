6a67f250a7a8e2b3b9c585e70db19a9f9b29be9e
aaakramar <aaakramar@gmail.com>
2024-11-13 08:46:28 +0530

    add function updated to handle different delimiters

--- app/utils/stringCalculator.js
+++ app/utils/stringCalculator.js
@@ -1,5 +1,14 @@
 export const add = (numbers) => {
+    console.log("numbers", numbers);
     if (numbers === "") return 0;
-    const numbersArray = numbers.split(/[,\n]+/);
+    let delimiter = /,|\n/;
+    if (numbers.startsWith("//")) {
+        const delimiterMatch = numbers.match(/^\/\/(.)\n/);
+        if (delimiterMatch) {
+            delimiter = new RegExp(delimiterMatch[1]);
+            numbers = numbers.slice(4);
+        }
+    }
+    const numbersArray = numbers.split(delimiter);
     return numbersArray.reduce((acc, num) => acc + parseInt(num), 0);
 };
--- app/utils/stringCalculator.test.js
+++ app/utils/stringCalculator.test.js
@@ -17,3 +17,8 @@ test("returns the sum accepting new lines as delimiter", () => {
     expect(add("1\n5,4")).toBe(10);
     expect(add("1\n2\n4")).toBe(7);
 })
+
+test("returns the sum accepting custom delimiters", () => {
+    expect(add("//;\n1;5")).toBe(6);
+    expect(add("//;\n1;5;4")).toBe(10);
+})
